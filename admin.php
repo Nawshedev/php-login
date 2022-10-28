c8b66a28158afb8e43c7fffc80f25b39ed7c29c9
Nawel Grine <nawelgrineb@gmail.com>
2022-10-28 14:23:46 +0200

    register + login + logout

---
+++ admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap Link -->
+    <!-- CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    <title>Admin</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand p-5" href="index.php">Accueil</a>
+        <a class="p-5" href="logout.php"> déconnexion </a>
+    </nav>
+    <h1 class="text-center mt-5">Utilisateurs enregistrés</h1>
+
+
+</body>
+
+</html>
--- register-ttt.php
+++ register-ttt.php
@@ -1,6 +1,6 @@
 <?php
 
-
+session_start();
 $database = new PDO('mysql:host=localhost;dbname=account;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 
@@ -9,28 +9,63 @@ if (isset($_POST["password"]) && isset($_POST["pseudo"])) {
     $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
     $pseudo = $_POST["pseudo"];
 
-    $regex = ^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$;
+    $regex = preg_match('/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z !"#$%&\'()*+,-.\/:;<=>?@\[\\\\\]^_`{|}~]{8,255}/', $_POST["password"]);
 
-    if ((!empty($pseudo)) && (!empty($password)) && (strlen("$password") > 8) && (preg_match($regex, $password))) {
+    if ((!empty($pseudo)) && (!empty($_POST["password"])) && $regex) {
 
         $checkPseudo = $database->prepare("SELECT * from user WHERE pseudo=?");
         $checkPseudo->execute([$pseudo]);
         $resultPseudo = $checkPseudo->rowCount();
         var_dump($resultPseudo);
-        if ($resultPseudo > 0) {
+        if ($resultPseudo > 1) {
             header("Location:register.php?error=Merci de choisir un autre pseudo.Celui-ci est déjà pris.");
         } else {
             $result = $database->prepare("INSERT INTO user(password, pseudo) VALUE(?, ?)");
             $result->execute([$password, $pseudo]);
-            echo "bravo, vous avez créé un compte";
+            $_SESSION['pseudo'] = $pseudo;
+            header("Location:dashboard.php");
         }
-    } else if (empty($pseudo) && empty($password)) {
-        header("Location:register.php?error=Merci de remplir tous les champs.");
-    } else if (empty($pseudo)) {
-        header("Location:register.php?error=Merci de choisir un pseudo");
-    } else if (empty($password) || strlen("$password") < 8) {
-        header("Location:register.php?error=Merci de choisir un mot de passe de plus de 8 caractères et avec une majuscule au minimum'");
+    } else {
+        if (empty($pseudo) && empty($_POST["password"])) {
+            header("Location:register.php?error=Merci de remplir tous les champs.");
+        } else if (empty($pseudo)) {
+            header("Location:register.php?error=Merci de choisir un pseudo");
+        } else if (empty($_POST["password"]) || !$regex) {
+            header("Location:register.php?error=Merci de choisir un mot de passe de plus de 8 caractères et avec une majuscule au minimum'");
+        }
     }
-} else {
-    echo "password and pseudo not set";
 }
+
+//admin mp : Motde-passe5ecur
+
+
+
+//     if ((!empty($pseudo)) && (!empty($password)) && $regex) {
+
+//         $checkPseudo = $database->prepare("SELECT * from user WHERE pseudo=?");
+//         $checkPseudo->execute([$pseudo]);
+//         $resultPseudo = $checkPseudo->rowCount();
+//         var_dump($resultPseudo);
+//         if ($resultPseudo > 0) {
+//             header("Location:register.php?error=Merci de choisir un autre pseudo.Celui-ci est déjà pris.");
+//         } else {
+//             if ((!empty($password)) && $regex) {
+//                 $result = $database->prepare("INSERT INTO user(password, pseudo) VALUE(?, ?)");
+//                 $result->execute([$password, $pseudo]);
+//                 echo "bravo, vous avez créé un compte";
+//             } else if (empty($pseudo) && empty($password)) {
+//                 header("Location:register.php?error=Merci de remplir tous les champs.");
+//             } else if (empty($pseudo)) {
+//                 header("Location:register.php?error=Merci de choisir un pseudo");
+//             } else if (empty($password) && $regex) {
+//                 header("Location:register.php?error=Merci de choisir un mot de passe de plus de 8 caractères et avec une majuscule au minimum'");
+//             }
+//         }
+//     } else if (empty($pseudo) || empty($password)) {
+//         header("Location:register.php?error=Merci de remplir tous les champs.");
+//     } else if (empty($pseudo)) {
+//         header("Location:register.php?error=Merci de choisir un pseudo");
+//     } else if (empty($password)) {
+//         header("Location:register.php?error=Merci de choisir un mot de passe de plus de 8 caractères et avec une majuscule au minimum'");
+//     }
+// }
